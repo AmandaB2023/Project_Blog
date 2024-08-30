96146094d7cfe637ce729825f4af361b2fd527b4
Amanda Fahy <amandafahy@ymail.com>
2024-08-30 21:31:56 +0000

    Add post model

--- project/settings.py
+++ project/settings.py
@@ -25,9 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-)$#^esurw0ki2tu8j9s@sx!x4e7krze#nivg7q)5^5$!)5i2s9'
-
+# SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+# DEBUG = os.environ.get("DEBUG") == 'True'
+
 
 ALLOWED_HOSTS = ['8000-amandab2023-projectblog-vtt4hsi67p9.ws-eu115.gitpod.io','.herokuapp.com']
 
@@ -87,6 +89,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*.herokuapp.com",
+    "https://*.codeinstitute-ide.net"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
